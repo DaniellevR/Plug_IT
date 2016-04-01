eb9fd7726a2885c8571b6ba882a8af44e41facb9
Daniëlle van Rooij <danielle.vanrooij94@gmail.com>
2016-04-01 22:39:29 +0200

    Tasks updated.

--- smarty/templates_c/76f9962107d00cefa60be80ed5efc0020b49bc71_0.file.tasks.tpl.php
+++ smarty/templates_c/76f9962107d00cefa60be80ed5efc0020b49bc71_0.file.tasks.tpl.php
@@ -1,13 +1,51 @@
-{extends file='layout.tpl'}
-{block name=body}
+<?php
+/* Smarty version 3.1.29, created on 2016-04-01 22:31:01
+  from "C:\wamp\www\Plug_IT\smarty\templates\tasks.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_56feda85b3e933_08307904',
+  'file_dependency' => 
+  array (
+    '76f9962107d00cefa60be80ed5efc0020b49bc71' => 
+    array (
+      0 => 'C:\\wamp\\www\\Plug_IT\\smarty\\templates\\tasks.tpl',
+      1 => 1459542623,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:layout.tpl' => 1,
+  ),
+),false)) {
+function content_56feda85b3e933_08307904 ($_smarty_tpl) {
+$_smarty_tpl->ext->_inheritance->init($_smarty_tpl, true);
+?>
+
+<?php 
+$_smarty_tpl->ext->_inheritance->processBlock($_smarty_tpl, 0, 'body', array (
+  0 => 'block_183956feda85b33937_58536515',
+  1 => false,
+  3 => 0,
+  2 => 0,
+));
+?>
+
+<?php $_smarty_tpl->ext->_inheritance->endChild($_smarty_tpl);
+$_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:layout.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
+}
+/* {block 'body'}  file:tasks.tpl */
+function block_183956feda85b33937_58536515($_smarty_tpl, $_blockParentStack) {
+?>
+
     <div class="content">
         <h1>Taakverdeling</h1>
         <h2>Samen</h2>
         <ul>
             <li>EER opgesteld</li>
             <li>Schets homepage</li>
-            <li>W3C gevalideerd</li>
-            <li>CSS verbeteren</li>
         </ul>
         <h2>Nigel</h2>
         <ul>
@@ -51,7 +89,9 @@
             <li>Registratiepagina</li>
             <li>Profielpagina</li>
             <li>Sitemap</li>
-            <li>Taakverdeling in site</li>
         </ul>
     </div>
-{/block}
+<?php
+}
+/* {/block 'body'} */
+}
