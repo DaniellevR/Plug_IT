3347ec70cbc7e067650185f280d0e68be7ae3282
MasTahFuLL <nabliebe@avans.nl>
2016-04-04 00:28:47 +0200

    bestellen vanuit product

--- smarty/templates_c/5d44fc100741d91acc8d75550c669e1a4aca0714_0.file.product.tpl.php
+++ smarty/templates_c/5d44fc100741d91acc8d75550c669e1a4aca0714_0.file.product.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-04-04 00:15:40
+/* Smarty version 3.1.29, created on 2016-04-04 00:25:22
   from "C:\xampp\htdocs\Plug_IT\smarty\templates\product.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5701960cce3246_24319049',
+  'unifunc' => 'content_57019852c18674_22828508',
   'file_dependency' => 
   array (
     '5d44fc100741d91acc8d75550c669e1a4aca0714' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Plug_IT\\smarty\\templates\\product.tpl',
-      1 => 1459435870,
+      1 => 1459722267,
       2 => 'file',
     ),
   ),
@@ -20,13 +20,13 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:layout.tpl' => 1,
   ),
 ),false)) {
-function content_5701960cce3246_24319049 ($_smarty_tpl) {
+function content_57019852c18674_22828508 ($_smarty_tpl) {
 $_smarty_tpl->ext->_inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
 $_smarty_tpl->ext->_inheritance->processBlock($_smarty_tpl, 0, 'body', array (
-  0 => 'block_16265701960ccd9df5_09510660',
+  0 => 'block_338257019852c0ca40_68463628',
   1 => false,
   3 => 0,
   2 => 0,
@@ -37,7 +37,7 @@ $_smarty_tpl->ext->_inheritance->processBlock($_smarty_tpl, 0, 'body', array (
 $_smarty_tpl->smarty->ext->_subtemplate->render($_smarty_tpl, "file:layout.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
 }
 /* {block 'body'}  file:product.tpl */
-function block_16265701960ccd9df5_09510660($_smarty_tpl, $_blockParentStack) {
+function block_338257019852c0ca40_68463628($_smarty_tpl, $_blockParentStack) {
 ?>
 
     <div class="content">
@@ -60,6 +60,15 @@ function block_16265701960ccd9df5_09510660($_smarty_tpl, $_blockParentStack) {
                 <p>€<?php echo $_smarty_tpl->tpl_vars['product']->value->price;?>
 
                 </p>
+            </div>
+            <div class="input">
+                <form action="/Plug_IT/index.php?page=Cart" method="post">
+                    <input name="amount" type="number" min="1" step="1" value="1" />
+                    <input name="page" value="Cart" hidden/>
+                    <input name="id" value="<?php echo $_smarty_tpl->tpl_vars['product']->value->id;?>
+" hidden/>
+                    <input id="addToCartButton" type="submit" value="Toevoegen aan winkelwagentje"/>
+                </form>
             </div>
         </div>
     </div>
